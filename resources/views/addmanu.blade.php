5e9fecad6289302f409f9b1f6ec9e9e8567da240
Thomas Stokes <89877059+RedlineRacer@users.noreply.github.com>
2022-03-30 13:01:50 -0700

    button

--- resources/views/addmanu.blade.php
+++ resources/views/addmanu.blade.php
@@ -4,5 +4,7 @@
     <input type="text" name="manufacturer" placeholder="Enter Manufacturer"> <br> </br>
     <input type="text" name="salescontact" placeholder="Enter Sales Contact Info"> <br> </br>
     <input type="text" name="techcontact" placeholder="Enter Tech Contact Info"> <br> </br>
+<button type="submit">Add Manufacturer</button>
+
 
 </form>
