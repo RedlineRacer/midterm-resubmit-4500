2c55b14e874177899c322b9de902af049a29e069
Thomas Stokes <89877059+RedlineRacer@users.noreply.github.com>
2022-03-30 17:14:48 -0700

    fix

--- app/Models/Manu.php
+++ app/Models/Manu.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\hasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Member extends Model
+class Manu extends Model
 {
     use HasFactory;
     public $timestamps=false;
